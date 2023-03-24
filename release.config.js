652546743fc1f8b0609cef4b6e53187f401dd93a
ryanduah <ryanbonty@hotmail.com>
2023-03-24 12:01:51 -0500

    fix: semantic versioning

--- release.config.js
+++ release.config.js
@@ -1,6 +1,6 @@
 module.exports = {
   branches: "main",
-  repositoryUrl: "https://github.com/Kenmakhanu/actionstest.git",
+  repositoryUrl: "https://github.com/ryanduah/AC.git",
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
