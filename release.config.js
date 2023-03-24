df0cafe6f99283af2268bde9f7328a2b58924cc8
ryanduah <ryanbonty@hotmail.com>
2023-03-24 10:30:26 -0500

    fix: semantic versioning

--- release.config.js
+++ release.config.js
@@ -1,9 +1,9 @@
 module.exports = {
-    branches: "main",
-    repositoryUrl: "git@github.com:Devops-Master-projects/Action_Test.git",
-    plugins: [
+   branches: "main",
+   repositoryUrl: "git@github.com:Devops-Master-projects/Action_Test.git",
+   plugins: [
       '@semantic-release/commit-analyzer',
       '@semantic-release/release-notes-generator',
       '@semantic-release/git',
       '@semantic-release/github']
-    }
+}
