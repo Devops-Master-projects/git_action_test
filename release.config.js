696bfea28a2ecdfd07bfdb742848720d11af5eae
ryanduah <ryanbonty@hotmail.com>
2023-03-24 13:06:36 -0500

    fix: export GH token key

--- release.config.js
+++ release.config.js
@@ -5,5 +5,6 @@ module.exports = {
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/git',
-    '@semantic-release/github']
+    '@semantic-release/github'],
+    "githubToken": "${GH_TOKEN}"
 }
