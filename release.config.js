60a17cc52c2256aa42fb7943ff8d6ac88a00d1ec
ryanduah <ryanbonty@hotmail.com>
2023-03-25 09:04:01 -0500

    fix:, semantic

--- release.config.js
+++ release.config.js
@@ -1,5 +1,5 @@
 module.exports = {
-  branches: "main", 
+  branches: "*", 
   repositoryUrl: "https://github.com/Devops-Master-projects/git_action_test.git",
   plugins: [
     '@semantic-release/commit-analyzer',
