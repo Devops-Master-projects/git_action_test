59679a8a83d52bf7edf29740f5a7153e42192811
ryanduah <ryanbonty@hotmail.com>
2023-03-25 06:15:34 -0500

    another one

--- release.config.js
+++ release.config.js
@@ -1,10 +1,10 @@
 module.exports = {
-  branches: "main",
-  repositoryUrl: "https://github.com/Devops-Master-projects/git_action_test",
+  branches: "master",
+  repositoryUrl: "https://github.com/Devops-Master-projects/git_action_test.git",
   plugins: [
     '@semantic-release/commit-analyzer',
     '@semantic-release/release-notes-generator',
     '@semantic-release/git',
     '@semantic-release/github'],
-    "githubToken": "${GH_TOKEN}"
 }
+
